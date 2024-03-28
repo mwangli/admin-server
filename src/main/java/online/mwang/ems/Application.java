dab12e31fbd6689e032b40ff1e1e3b4de7249632
mwangli <1325533186@qq.com>
2024-03-28 11:37:24 +0800

    init

--- src/main/java/online/mwang/ems/Application.java
+++ src/main/java/online/mwang/ems/Application.java
@@ -2,12 +2,12 @@ package online.mwang.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author mwangli
  */
-
 @EnableSwagger2
 @SpringBootApplication
 public class Application {
